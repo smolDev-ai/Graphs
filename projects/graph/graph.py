befe4bfc339f70c0e3f90f0f752c93b729a30f84
smolDev-ai <driahthedragonwalker@gmail.com>
2020-03-16 14:14:38 -0500

    DFS_Recursive done -- super similar to the iterative versions.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -79,7 +79,7 @@ class Graph:
         This should be done using recursion.
         """
 
-        if starting_vertex not in visited and starting_vertex is not None:
+        if starting_vertex not in visited:
             visited.add(starting_vertex)
             print(starting_vertex)
 
@@ -172,7 +172,7 @@ class Graph:
                     for neighbor in self.get_neighbors(current_node):
                         stack.push([*current_path, neighbor])
 
-    def dfs_recursive(self, starting_vertex):
+    def dfs_recursive(self, starting_vertex, destination_vertex, visited=set()):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
@@ -180,7 +180,22 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        
+        if starting_vertex not in visited:
+            visited.add(starting_vertex)
+
+            if starting_vertex == destination_vertex:
+                return [starting_vertex]
+            else:
+                for neighbor in self.get_neighbors(starting_vertex):
+                    path = self.dfs_recursive(neighbor, destination_vertex, visited)
+
+                    if path is not None and destination_vertex in path:
+                        return [starting_vertex, *path]
+                    
+
+
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
@@ -248,4 +263,4 @@ if __name__ == '__main__':
         [1, 2, 4, 7, 6]
     '''
     # print(graph.dfs(1, 6))
-    # print(graph.dfs_recursive(1, 6))
+    print(graph.dfs_recursive(1, 6))
